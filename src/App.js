e9c4ba1eedd56ff49d5334500a4170749c607159
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-09-11 10:50:59 +0700

    Updated files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/Menu';
+import { DISHES } from './shared/dishes'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return(
       <div>
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <NavbarBrand  className="fw-bolder" href="/">Ristorante Con Fusion</NavbarBrand>
           </div>    
         </Navbar>
-        <Menu />
+        <Menu dishes = {this.state.dishes} />
       </div>
     );
   }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,74 +1,57 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Menu extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      dishes: [
-        {
-          id: 0,
-          name: "Uthappizza",
-          image: "./assets/images/uthappizza.png",
-          category: "mains",
-          label: "hot",
-          price: "4.99",
-          description:"A unique combination of Indian Uthappam (pancake) and Italian pizza. topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Panneer.",
-        },
-        {
-          id: 1,
-          name: "Zucchipakoda",
-          image: "./assets/images/zucchipakoda.png",
-          category: "appetier",
-          label: "",
-          price: "1.99",
-          description:"Deep fried zucchini coated with mildy spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce.",
-        },
-        {
-          id: 2,
-          name: "Vadonut",
-          image: "./assets/images/vadonut.png",
-          category: "appetier",
-          label: "New",
-          price: "1.99",
-          description:"A quintessential Confusion experience, is it a vada or is it a donut ?",
-        },
-        {
-          id: 3,
-          name: "ElaiCheese Cake",
-          image: "./assets/images/elaicheesecake.png",
-          category: "appetier",
-          label: "",
-          price: "1.99",
-          description:"A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamos.",
-        },
-      ],
+      selectedDish: null,
     };
+
   }
 
-  render(){
-    const menu = this.state.dishes.map(dish => {
+  onDishSelect(dish) {
+    this.setState({ selectedDish: dish });
+  }
+
+  renderDish(dish) {
+    if(dish != null) {
+      return(
+        <Card>
+          <CardImg top src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle className="fw-bold">{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      )
+    }
+  }
+
+  render() {
+    const menu = this.props.dishes.map((dish) => {
       return (
-        <div key={dish.id} className="col-12 mt-5">
-          <Media tag="li" className="d-flex flex-row align-items-center gap-3">
-            <Media>
-              <Media object src={dish.image} alt={dish.name}/>
-            </Media>
-            <Media body>
-              <Media heading>{dish.name}</Media>
-              <p>{dish.description}</p>
-            </Media>
-          </Media>
+        <div className=" p-2 flex-fill">
+          <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+              <CardTitle heading className="border border-primary rounded-3 bg-primary bg-opacity-75 w-50 fw-bold text-white text-wrap text-center">
+                {dish.name}
+              </CardTitle>
+            </CardImgOverlay>
+          </Card>
         </div>
       );
     });
 
     return (
       <div className="container">
-        <div className="row"> 
-          <Media list>
-            {menu}
-          </Media>
+        <div className="d-flex flex-row flex-wrap">{menu}</div>
+        <div className="row">
+          <div className="col-12 col-md-5 m-2">
+            {this.renderDish(this.state.selectedDish)}
+          </div>
         </div>
       </div>
     );

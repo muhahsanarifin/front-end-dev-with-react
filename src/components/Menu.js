099e1cc6baed6274a11afcae1929029a71a6c297
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-09-12 13:02:39 +0700

    Updated files

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -29,6 +29,42 @@ class Menu extends Component {
     }
   }
 
+  renderComments(dish) {
+    if(dish != null) {
+      const coms = dish.comments.map(com => {
+        return (
+          <ul key={com.id} className="list-unstyled">
+            <li className="text-primary fw-bold">
+              Rating: {com.rating}
+            </li>
+            <li>
+              {com.comment}
+            </li>
+            <li>
+              - {com.author}, {}
+              {
+                new Intl.DateTimeFormat('en-US', {
+                  month: 'short', day: '2-digit', year: 'numeric'
+                }).format(new Date(com.date))
+              }
+            </li>
+          </ul>
+        )
+      });
+
+      return(
+        <div className="p-2">
+          <h3>Comments</h3>
+          {coms}
+        </div>
+      )
+    } else {
+      return(
+        <div></div>
+      )
+    }
+  }
+
   render() {
     const menu = this.props.dishes.map((dish) => {
       return (
@@ -48,9 +84,12 @@ class Menu extends Component {
     return (
       <div className="container">
         <div className="d-flex flex-row flex-wrap">{menu}</div>
-        <div className="row">
+        <div className="d-flex flex-row flex-wrap justify-content-center px-2">
           <div className="col-12 col-md-5 m-2">
             {this.renderDish(this.state.selectedDish)}
+          </div>
+          <div className="col-12 col-md-5 m-2">
+            {this.renderComments(this.state.selectedDish)}
           </div>
         </div>
       </div>
